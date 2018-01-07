5184586c18e5cf1cf5cece10d1fc67b3abfefd1f
andresshare <andresshare@gmail.com>
2018-01-06 19:16:32 -0500

    Updated status to blog Enum

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180106215328) do
+ActiveRecord::Schema.define(version: 20180107000431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20180106215328) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0
   end
 
   create_table "portfolios", force: :cascade do |t|
